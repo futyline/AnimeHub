7fff1375cefd0562c1c1b7ae28235e0a903d34fe
Lierbeth De Carvalho <84059040+futyline@users.noreply.github.com>
2024-01-20 22:45:44 -0300

    Update animes.js

--- animes.js
+++ animes.js
@@ -1,44 +1,42 @@
-const animes = [
+[
   {
-    title: "Ao No Exorcist 3",
-    path: "ao-no-exorcist-3",
-    episodes: 12,
-    letra: "a",
-    player: 1,
-    msg: "",
+    "title": "Ao No Exorcist 3",
+    "path": "ao-no-exorcist-3",
+    "episodes": 12,
+    "letra": "a",
+    "player": 1,
+    "msg": ""
   },
   {
-    title: "Chainsaw Man",
-    path: "chainsaw-man",
-    episodes: 12,
-    letra: "c",
-    player: 1,
-    msg: "Episodio 01 e 02 indisponiveis",
+    "title": "Chainsaw Man",
+    "path": "chainsaw-man",
+    "episodes": 12,
+    "letra": "c",
+    "player": 1,
+    "msg": "Episódio 01 e 02 indisponíveis"
   },
   {
-    title: "Jujutsu Kaisen",
-    path: "jujutsu-kaisen",
-    episodes: 24,
-    letra: "j",
-    player: 2,
-    msg: "",
+    "title": "Jujutsu Kaisen",
+    "path": "jujutsu-kaisen",
+    "episodes": 24,
+    "letra": "j",
+    "player": 2,
+    "msg": ""
   },
   {
-    title: "Jujutsu Kaisen 2",
-    path: "jujutsu-kaisen-2",
-    episodes: 23,
-    letra: "j",
-    player: 1,
-    msg: "",
+    "title": "Jujutsu Kaisen 2",
+    "path": "jujutsu-kaisen-2",
+    "episodes": 23,
+    "letra": "j",
+    "player": 1,
+    "msg": ""
   },
   {
-    title: "Solo Leveling",
-    path: "solo-leveling",
-    episodes: 25,
-    letra: "s",
-    player: 1,
-    msg: "",
-  },
-];
-
-export default animes;
+    "title": "Solo Leveling",
+    "path": "solo-leveling",
+    "episodes": 25,
+    "letra": "s",
+    "player": 1,
+    "msg": ""
+  }
+]
