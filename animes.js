84bb7e66c873337599e68e0ceb37566d233cb9df
Lierbeth De Carvalho <84059040+futyline@users.noreply.github.com>
2024-01-21 00:21:28 -0300

    Update animes.js

--- animes.js
+++ animes.js
@@ -23,6 +23,15 @@
     "player": 2,
     "msg": "O Melhor Anime da historia"
   },
+ {
+    "title": "Hunter x Hunter 2011",
+    "path": "hunterx-x-hunter-2011",
+    "episodes": 148,
+    "letra": "h",
+    "player": 2,
+    "msg": ""
+  },
+
 
   {
     "title": "Jujutsu Kaisen",
@@ -46,6 +55,48 @@
     "episodes": 25,
     "letra": "s",
     "player": 1,
+    "msg": ""
+  },
+
+
+{
+    "title": "Sword Art Online",
+    "path": "sword-art-online",
+    "episodes": 25,
+    "letra": "s",
+    "player": 2,
+    "msg": ""
+  },{
+    "title": "Sword Art Online 2",
+    "path": "sword-art-online-ii",
+    "episodes": 24,
+    "letra": "s",
+    "player": 2,
+    "msg": ""
+  },
+
+  {
+    "title": "Sword Art Online 3 Alicization",
+    "path": "sword-art-online-alicization",
+    "episodes": 25,
+    "letra": "s",
+    "player": 2,
+    "msg": ""
+  },
+  {
+    "title": "Sword Art Online 3 Alicization War of Underworld",
+    "path": "sword-art-online-alicization-war-of-underworld",
+    "episodes": 12,
+    "letra": "s",
+    "player": 2,
+    "msg": ""
+  },
+  {
+    "title": "Sword Art Online 3 Alicization War of Underworld 2",
+    "path": "sword-art-online-alicization-war-of-underworld-2",
+    "episodes": 11,
+    "letra": "s",
+    "player": 2,
     "msg": ""
   }
 ]
