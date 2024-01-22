960d5f422968344be610c6d3efc2627877df0566
Lierbeth De Carvalho <84059040+futyline@users.noreply.github.com>
2024-01-22 19:41:03 -0300

    Update animes.js

--- animes.js
+++ animes.js
@@ -5,23 +5,26 @@
     "episodes": 12,
     "letra": "a",
     "player": 1,
-    "msg": ""
+    "msg": "",
+    "img": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiUccRM1lMTXtoZjcCG2_AOd3N0VrmDIMlSNuTX5yPud5KLpme5lAdSJgou-YjPJLiwBAXqssQtuRmW_TloON8WMpKZuTtlLDqAWg3IxXBXE6FwzH9Ump-zEPfsse-eL1DalBnlwqv-zyuDzpdgqVEohtr4IzR-pu6kUs6_5jClKwdE2bUhrs4zS0CC/s600/FotoJet.png"
   },
-{
+  {
     "title": "Another",
     "path": "another",
     "episodes": 13,
     "letra": "a",
     "player": 2,
-    "msg": ""
+    "msg": "",
+    "img": "https://upload.wikimedia.org/wikipedia/en/4/4c/Another_series_cover.jpg"
   },
- {
+  {
     "title": "Ansatsu Kyoushitsu (Assassination classroom)",
     "path": "ansatsu-kyoushitsu",
     "episodes": 22,
     "letra": "a",
     "player": 2,
-    "msg": ""
+    "msg": "",
+    "img": "https://upload.wikimedia.org/wikipedia/pt/a/a8/Assassination_Classroom_1.jpg"
   },
   {
     "title": "Ansatsu Kyoushitsu 2 (Assassination classroom)",
@@ -29,7 +32,8 @@
     "episodes": 25,
     "letra": "a",
     "player": 2,
-    "msg": ""
+    "msg": "",
+    "img": "https://www.animenewsnetwork.com/hotlink/images/encyc/A17686-1027996516.1451462689.jpg"
   },
   {
     "title": "Boku Dake ga inai Machi (Erased)",
@@ -37,16 +41,18 @@
     "episodes": 12,
     "letra": "b",
     "player": 3,
-    "num" :"010",
-    "msg": ""
+    "num": "010",
+    "msg": "",
+    "img": "https://i.pinimg.com/originals/e0/e4/f8/e0e4f83efbe626ec686d2b074cb40ab9.jpg"
   },
-{
+  {
     "title": "Boku no Hero Academia",
     "path": "boku-no-hero",
     "episodes": 13,
     "letra": "b",
     "player": 2,
-    "msg": ""
+    "msg": "",
+    "img": "https://i.pinimg.com/originals/78/dd/2c/78dd2c58458f01d7ee741ed8d9f9f4b5.jpg"
   },
 
   {
@@ -55,7 +61,8 @@
     "episodes": 25,
     "letra": "b",
     "player": 2,
-    "msg": ""
+    "msg": "",
+    "img": "https://static.wikia.nocookie.net/bokunoheroacademia/images/7/7f/Season_2_Poster_2.png/revision/latest?cb=20170407221323"
   },
   {
     "title": "Boku no Hero Academia 3",
@@ -63,7 +70,8 @@
     "episodes": 25,
     "letra": "b",
     "player": 2,
-    "msg": ""
+    "msg": "",
+    "img": "https://static.wikia.nocookie.net/bokunoheroacademia/images/b/b1/Season_3_Poster_1.png/revision/latest/scale-to-width/360?cb=20180219032852"
   },
   {
     "title": "Boku no Hero Academia 4",
@@ -71,7 +79,8 @@
     "episodes": 25,
     "letra": "b",
     "player": 2,
-    "msg": ""
+    "msg": "",
+    "img": "https://cdn.animenewsnetwork.com/thumbnails/max700x700/cms/news.3/150875/visual.jpg"
   },
   {
     "title": "Boku no Hero Academia 5",
@@ -79,7 +88,8 @@
     "episodes": 25,
     "letra": "b",
     "player": 2,
-    "msg": ""
+    "msg": "",
+    "img": "https://static.wikia.nocookie.net/bokunoheroacademia/images/3/39/Season_5_Poster.png/revision/latest?cb=20210814232856&path-prefix=pt-br"
   },
   {
     "title": "Boku no Hero Academia 6",
@@ -87,7 +97,8 @@
     "episodes": 25,
     "letra": "b",
     "player": 1,
-    "msg": ""
+    "msg": "",
+    "img": "https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/251524e3b5517b689317437d881eccf0.jpe"
   },
   {
     "title": "Chainsaw Man",
@@ -95,7 +106,8 @@
     "episodes": 12,
     "letra": "c",
     "player": 1,
-    "msg": "Episódio 01 e 02 indisponíveis"
+    "msg": "Episódio 01 e 02 indisponíveis",
+    "img": "https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/922742d9acaeba7d887ed11b6caab0e4.jpeg"
   },
   {
     "title": "Fullmetal Alchemist Brotherhood",
@@ -103,7 +115,8 @@
     "episodes": 64,
     "letra": "f",
     "player": 2,
-    "msg": "O Melhor Anime da historia"
+    "msg": "O Melhor Anime da historia",
+    "img": "https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/0662921aa3b81ff85737ddeb56deefab.jpeg"
   },
   {
     "title": "Hunter x Hunter 2011",
@@ -123,7 +136,6 @@
     "player": 2,
     "msg": "",
     "img": "https://static.wikia.nocookie.net/jujutsu-kaisen/images/8/88/Anime_Key_Visual_2.png/revision/latest?cb=20201212034001"
-
   },
   {
     "title": "Jujutsu Kaisen 2",
@@ -133,25 +145,26 @@
     "player": 1,
     "msg": "",
     "img": "https://images.justwatch.com/poster/306336521/s592/temporada-2"
-
   },
- {
+  {
     "title": "One Punch Man",
     "path": "one-punch-man",
     "episodes": 12,
     "letra": "o",
     "player": 3,
-    "num":"010",
-    "msg": ""
+    "num": "010",
+    "msg": "",
+    "img": "https://m.media-amazon.com/images/I/71Fh8JX7B8L._AC_UF894,1000_QL80_.jpg"
   },
- {
+  {
     "title": "One Punch Man 2",
     "path": "one-punch-man-2",
     "episodes": 14,
     "letra": "o",
     "player": 3,
-    "num":"010",
-    "msg": ""
+    "num": "010",
+    "msg": "",
+    "img": "https://egoamo.co.za/cdn/shop/products/OnePunchMan-Season2AnimePosterEgoamo.co.zaPosters_530x@2x.jpg?v=1623083566"
   },
   {
     "title": "Shingeki No Kyojin (Attack on titan)",
@@ -159,7 +172,8 @@
     "episodes": 25,
     "letra": "s",
     "player": 2,
-    "msg": ""
+    "msg": "",
+    "img": "https://static.wikia.nocookie.net/shingekinokyojin/images/d/d8/Attack_on_Titan_Season_1.jpg/revision/latest?cb=20211005182832"
   },
   {
     "title": "Shingeki No Kyojin 2 (Attack on titan)",
@@ -167,7 +181,8 @@
     "episodes": 12,
     "letra": "s",
     "player": 2,
-    "msg": ""
+    "msg": "",
+    "img": "https://i.pinimg.com/736x/81/f5/c9/81f5c9a8f206b2b4389adb25625b02cf.jpg"
   },
   {
     "title": "Shingeki No Kyojin 3 parte 1 (Attack on titan)",
@@ -175,7 +190,8 @@
     "episodes": 12,
     "letra": "s",
     "player": 2,
-    "msg": ""
+    "msg": "",
+    "img": "https://imusic.b-cdn.net/images/item/original/941/5022366767941.jpg?anime-2021-attack-on-titan-season-3-dvd&class=scaled&v=1645978017"
   },
   {
     "title": "Shingeki No Kyojin 3 parte 2 (Attack on titan)",
@@ -183,7 +199,8 @@
     "episodes": 10,
     "letra": "s",
     "player": 2,
-    "msg": ""
+    "msg": "",
+    "img": "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx104578-LaZYFkmhinfB.jpg"
   },
   {
     "title": "Shingeki No Kyojin 4 parte 1 (Attack on titan)",
@@ -191,15 +208,17 @@
     "episodes": 16,
     "letra": "s",
     "player": 2,
-    "msg": ""
+    "msg": "",
+    "img": "https://wallpapercave.com/wp/wp10506837.jpg"
   },
   {
     "title": "Shingeki No Kyojin 4 parte 2 (Attack on titan)",
     "path": "shingeki-no-kyojin-the-final-season-2",
     "episodes": 12,
-    "letra": "s", 
+    "letra": "s",
     "player": 2,
-    "msg": ""
+    "msg": "",
+    "img": "https://media.fstatic.com/5kN9TjJjaQNJPnM2N2oN0eeryW4=/210x312/smart/filters:format(webp)/media/movies/covers/2020/12/EoePMKWVgAAA_LJ.jpeg"
   },
   {
     "title": "Shingeki No Kyojin 4 parte 3 final (Attack on titan)",
@@ -207,8 +226,9 @@
     "episodes": 2,
     "letra": "s",
     "player": 1,
-    "esp":"sp",
-    "msg": ""
+    "esp": "sp",
+    "msg": "",
+    "img": "https://i0.wp.com/ptanime.com/wp-content/uploads/2023/01/Shingeki-no-Kyojin-The-Final-Season-cover-anime.jpg?resize=700%2C992&ssl=1"
   },
   {
     "title": "Solo Leveling",
@@ -216,7 +236,8 @@
     "episodes": 25,
     "letra": "s",
     "player": 1,
-    "msg": ""
+    "msg": "",
+    "img": "https://m.media-amazon.com/images/M/MV5BODJkZTM3MWYtOTkxNS00YWUxLTg5NjAtOTk4ZWM5MTBmMzAyXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_FMjpg_UX1000_.jpg"
   },
 
   {
@@ -225,7 +246,8 @@
     "episodes": 25,
     "letra": "s",
     "player": 2,
-    "msg": ""
+    "msg": "",
+    "img": "https://m.media-amazon.com/images/M/MV5BYjY4MDU2YjMtNzY1MC00ODg1LWIwMzYtMWE5YTA3YTI4ZjMxXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
   },
   {
     "title": "Sword Art Online 2",
@@ -233,7 +255,8 @@
     "episodes": 24,
     "letra": "s",
     "player": 2,
-    "msg": ""
+    "msg": "",
+    "img": "https://media.fstatic.com/WoXgVsW7fvKrlapj6lyHyNrazes=/322x478/smart/filters:format(webp)/media/movies/covers/2014/07/sword-art-online-2a-temporada_t91903.jpg"
   },
 
   {
@@ -242,7 +265,8 @@
     "episodes": 25,
     "letra": "s",
     "player": 2,
-    "msg": ""
+    "msg": "",
+    "img": "https://upload.wikimedia.org/wikipedia/pt/f/f3/SAO_Alicization_Key_Visual.jpg"
   },
   {
     "title": "Sword Art Online 3 Alicization War of Underworld",
@@ -250,7 +274,8 @@
     "episodes": 12,
     "letra": "s",
     "player": 2,
-    "msg": ""
+    "msg": "",
+    "img": "https://doublesama.com/wp-content/uploads/2020/02/SAO-Alicization-War-of-Underworld-cover.jpg"
   },
   {
     "title": "Sword Art Online 3 Alicization War of Underworld 2",
@@ -258,7 +283,8 @@
     "episodes": 11,
     "letra": "s",
     "player": 2,
-    "msg": ""
+    "msg": "",
+    "img": "https://animetvonline.cx/wp-content/uploads/2020/07/1593599352-105106.jpg"
   },
   {
     "title": "The God of High School",
@@ -267,7 +293,8 @@
     "letra": "t",
     "player": 3,
     "num": "010",
-    "msg": ""
+    "msg": "",
+    "img": "https://static.wikia.nocookie.net/godofhighschool/images/a/a4/Anime_Key_Visual_1.png/revision/latest?cb=20200707220956"
   },
   {
     "title": "Zom 100: Bucket List of the Dead",
@@ -276,6 +303,7 @@
     "letra": "z",
     "player": 1,
     "num": "010",
-    "msg": ""
+    "msg": "",
+    "img": "https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/dd37db90df46fca8d702266385a1193c.jpe"
   }
 ]
