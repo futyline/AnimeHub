b3e5e9884c276b0c165d935ba11a1fc9406cdbd7
Lierbeth De Carvalho <futyline@gmail.com>
2024-03-12 09:07:33 -0300

    add naruto classico dublado

--- animesDublado.js
+++ animesDublado.js
@@ -13,5 +13,20 @@
             "img": "https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/e60626c56bf7437857e0caf7cbc4bf4a.jpe"
           }
         ]
+      },
+      {
+        "name": "Naruto Classico Dublado",
+        "defaultPicture": "https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/8532171bec0d05bfe45769a330fbab82.jpe",
+        "temporadas": [
+          {
+            "path": "naruto-classico-dublado",
+            "letra": "n",
+            "episodes": 220,
+            "title": "Naruto Classico Dublado",
+            "num": "010",
+            "player": 1,
+            "img": "https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/8532171bec0d05bfe45769a330fbab82.jpe"
+          }
+        ]
       }
 ]
